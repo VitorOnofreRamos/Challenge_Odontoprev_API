0b091966e0b7a9bc57a7223dd7f2a0f958ba266b
Vitor Onofre Ramos <107143879+VitorOnofreRamos@users.noreply.github.com>
2025-03-12 19:42:56 -0300

    Add project files.

---
+++ Challenge_Odontoprev_API/Models/Paciente.cs
@@ -0,0 +1,13 @@
+﻿namespace Challenge_Odontoprev_API.Models
+{
+    public class Paciente
+    {
+        public long ID_Paciente { get; set; }
+        public string Nome { get; set; }
+        public DateTime Data_Nascimento { get; set; }
+        public string CPF { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public long Carteirinha { get; set; }
+    }
+}
